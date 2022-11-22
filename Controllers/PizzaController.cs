501aef0bb97d27e85737b4ed1d99ab573a536d71
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-22 14:24:39 +0100

    creato il create

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,15 +1,22 @@
 ﻿using la_mia_pizzeria_static.Data;
 using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System.Data;
 
 namespace la_mia_pizzeria_static.Controllers
 {
+    
     public class PizzaController : Controller
     {
-        public IActionResult Index()
+        private PizzaDbContext db;
+
+        public PizzaController()
         {
-            PizzaDbContext db = new PizzaDbContext();
+            db = new PizzaDbContext();
+        }
+            public IActionResult Index()
+        {
             List<Pizza> Pizze = db.Pizzas.ToList();
             ViewData["Title"] = "Todi Pizza";
             return View(Pizze);
@@ -17,10 +24,26 @@ namespace la_mia_pizzeria_static.Controllers
 
         public IActionResult Detail(int id)
         {
-            PizzaDbContext db = new PizzaDbContext();
             Pizza pizza = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
             ViewData["Title"] = "Todi Pizza | " + pizza.Name;
             return View(pizza);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizzas.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_static.Models
@@ -6,10 +7,13 @@ namespace la_mia_pizzeria_static.Models
     public class Pizza
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         public string Name { get; set; }
         [Column(TypeName = "text")]
         public string Description { get; set; }
         public string Image { get; set; }
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Range(0.0, 1000.0, ErrorMessage = "Il prezzo non può essere negativo")]
         public double Price { get; set; }
         public Pizza()
         {
