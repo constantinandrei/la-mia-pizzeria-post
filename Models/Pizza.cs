6a0f3b021e3901ad320bf9cae372eee3fefd4a05
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-22 15:05:49 +0100

    esercizio finito

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using il_mio_primo_blog.Validator;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,12 +8,17 @@ namespace la_mia_pizzeria_static.Models
     public class Pizza
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo nome è obbligatorio")]
+        [StringLength(50, ErrorMessage ="Il nome può essere massimo 50 caratteri")]
         public string Name { get; set; }
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        [StringLength(300, ErrorMessage = "La descrizione può essere massimo 300 caratteri")]
+        [MinFiveWords]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Il campo immagine è obbligatorio")]
         public string Image { get; set; }
-        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Required(ErrorMessage = "Il campo prezzo è obbligatorio")]
         [Range(0.0, 1000.0, ErrorMessage = "Il prezzo non può essere negativo")]
         public double Price { get; set; }
         public Pizza()
---
+++ Validator/MinFiveWordsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace il_mio_primo_blog.Validator
+{
+    public class MinFiveWordsAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string fieldValue = (string)value;
+
+            if (fieldValue.Trim().Count(c => c == ' ') < 4)
+            {
+                return new ValidationResult("Il campo deve contenere almeno cinque parole");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
